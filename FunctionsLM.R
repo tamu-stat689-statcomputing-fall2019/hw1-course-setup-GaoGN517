3aafc4b95b89ebf612ac6200145f12c22dbc8d64
GN GAO <luyeqing517@gmail.com>
2019-08-31 23:25:15 -0500

    Create the function to return Y.

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -1,10 +1,18 @@
-# Generate n-dimensional response Y that follows linear regression model Y = Xbeta + epsilon, where epsilon is normal zero with variance sigma^2 independent across samples. Seed should be set at the beginning of the function
+# Generate n-dimensional response Y that follows linear regression model 
+# Y = Xbeta + epsilon, where epsilon is normal zero with variance sigma^2 
+# independent across samples. Seed should be set at the beginning of the function
 # X - design matrix
 # beta - given parameter vector
 # sigma - standard deviation of the noise
 # seed  - starting seed value
 generateY <- function(X, beta, sigma, seed = 5832652){
   #[ToDo] Set seed and generate Y following linear model
+  set.seed(seed)
+  
+  # Get length of Y
+  n <- nrow(X)
+  # Generate Y
+  Y <- rnorm(n, mean = crossprod(X, beta), sd = sigma)
   
   # Return Y
   return(Y)
