0f121adff765247f8824d7ac3689c7e579f1917f
GN GAO <luyeqing517@gmail.com>
2019-08-31 23:58:21 -0500

    Finish testing generateY function.

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -9,7 +9,7 @@ beta = rep(2,p) # true vector of coefficients
 X = matrix(rnorm(n*p), n, p) # n by p matrix of predictors
 
 # [ToDo] Use generateY function to generate Y
-
+Y = generateY(X, beta, sigma)
 # [ToDo] Use calculateBeta function to calculate beta_LS
 
 # [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
